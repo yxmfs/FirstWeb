7119c2eb3ee4f599c5238d31e843c700f9ef26a2
yxmfs <ygm@ubuntu.com>
2019-03-21 22:00:26 -0700

    helloworld url

--- FirstWeb/urls.py
+++ FirstWeb/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+ 
+from . import view
+ 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    url(r'^$', view.hello),
 ]
